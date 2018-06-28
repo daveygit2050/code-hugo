943d18d46fb14c1f439c9fbcb56dd741019f7c09
Dave Randall <dave@goldsquare.co.uk>
2018-06-28 18:34:51 +0100

    Removing site specific settings

--- acrelandscapes.org/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "goldsquare-state"
-    key    = "terraform/code-hugo/acrelandscapes.org"
-    region = "eu-west-1"
-  }
-}
--- acrelandscapes.org/providers.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  alias   = "${var.fqdn}"
-  profile = "${var.environment_profile}"
-  region  = "${var.environment_region}"
-}
-
-provider "aws" {
-  region = "${var.state_region}"
-}
